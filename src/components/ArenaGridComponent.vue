960d845e24f4aa397737698c65332e781f00a36f
Angela <angelaavalos2410@gmail.com>
2024-04-10 19:22:06 +0200

    cambios

--- src/components/ArenaGridComponent.vue
+++ src/components/ArenaGridComponent.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex flex-row justify-center m-">
-    <section class="grid grid-rows-5 grid-flow-col gap-5">
+  <section class="flex flex-row justify-center m-">
+    <div class="grid grid-rows-5 grid-flow-col gap-5">
       <section
         class="bg-steel text-white p-10 rounded-lg shadow-lg flex flex-col items-center"
         v-for="index in Count"
         :key="index"
       ></section>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
