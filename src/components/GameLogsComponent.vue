4e33be5a34ed7f3c08b8257e43501f9d4bb55017
Angela <angelaavalos2410@gmail.com>
2024-04-17 18:45:52 +0200

    Cambio de tags

--- src/components/GameLogsComponent.vue
+++ src/components/GameLogsComponent.vue
@@ -3,15 +3,15 @@ import GameReplayComponent from './GameReplayComponent.vue';
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
-    <form class="flex flex-col justify-start">
+  <form class="flex flex-col min-h-screen">
+    <div class="flex flex-col justify-start">
       <section class="grid gap-x-8 gap-y-4 grid-cols-1">
         <section v-for="index in playerInfoCount" :key="index">
           <GameReplayComponent />
         </section>
       </section>
-    </form>
-  </div>
+    </div>
+  </form>
 </template>
 
 <script>
